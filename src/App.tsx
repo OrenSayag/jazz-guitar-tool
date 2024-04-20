4771607ca8e44e48994d8c250013750727667cee
Oren Sayag <oren.s@techmarketing.co.il>
2024-04-20 13:03:46 +0700

    📦 added shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { Button } from "@/components/ui/button";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <div className={"fixed bottom-2 left-2"}>
+          <ModeToggle />
+        </div>
+        <Button variant={"default"}>Hello shadcn</Button>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
